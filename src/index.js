9b0a41c74d86afb5a4d5e7a7b551f332e045e0cc
Zhell <35771353+Zhell1@users.noreply.github.com>
2021-06-05 12:36:29 +0200

    vanilla js sample to start from

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import $ from "jquery";
+
+const rootApp = document.getElementById("root");
+
+window.clicked = function () {
+  console.log("clicked");
+  var button = document.getElementById("mybutton");
+  button.innerHTML = "clicked";
+};
+
+rootApp.innerHTML = `
+  <button id="mybutton" onclick="clicked()">
+  ON
+  </button>`;
